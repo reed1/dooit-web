b4daf31f3ba90996cb7e173edd8a146e31f73fcb
Ridwan Mulyadi <ridwan.mulyadi@live.com>
2025-05-11 05:54:11 +0700

    fix

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,7 +1,7 @@
 import { defineEventHandler } from 'h3';
 
 export default defineEventHandler(async (event) => {
-  if (event.path === '/login') {
+  if (event.path === '/login' || event.path === '/api/auth/login') {
     return;
   }
   const session = await getUserSession(event);
